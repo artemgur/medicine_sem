3cff1dedc49cf06ddafde028f54a31c6fff2609c
artemgur <artemgur01@gmail.com>
2020-11-03 16:38:03 +0300

    Progress in frontend

---
+++ Sem/Sem/ViewComponents/IndexArticleViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sem
+{
+	public class IndexArticleViewComponent:ViewComponent
+	{
+		public IndexArticleViewComponent()
+		{
+			
+		}
+		
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View("IndexArticleView");
+		}
+	}
+}
