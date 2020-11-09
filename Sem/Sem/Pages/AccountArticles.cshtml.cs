1bb081c28869cdd6cf2dc979a56b677fc9303ead
artemgur <artemgur01@gmail.com>
2020-11-09 15:30:52 +0300

    Mostly done account page

---
+++ Sem/Sem/Pages/AccountArticles.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sem.Pages
+{
+	public class AccountArticles : PageModel
+	{
+		public void OnGet()
+		{
+			
+		}
+	}
+}
---
+++ Sem/Sem/ViewComponents/AccountSidebar.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sem
+{
+	public class AccountSidebar:ViewComponent
+	{
+		public AccountSidebar()
+		{
+			
+		}
+		
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View("AccountSidebarView");
+		}
+	}
+}
