836762bc64b648652e7b8ba9abf4c877f0769539
Timur2363V <efwfw.ffew@mail.ru>
2020-12-14 14:09:56 +0300

    DBDelArticleShortDes

--- Sem/Sem/ModelsTables/Article.cs
+++ Sem/Sem/ModelsTables/Article.cs
@@ -12,10 +12,9 @@ namespace Sem.ModelsTables
             Title = articleParams[0].ToString();
             Image = articleParams[1] == null || articleParams[1].ToString() == "" ? "/img/placeholder.jpg" : articleParams[1].ToString();
             Description = articleParams[2].ToString();
-            Short_description = articleParams[3].ToString();
-            var setDate = articleParams[4].ToString().Split(' ', ':', '.').Select(x => int.Parse(x)).ToArray();
+            var setDate = articleParams[3].ToString().Split(' ', ':', '.').Select(x => int.Parse(x)).ToArray();
             Date = new DateTime(setDate[2], setDate[1], setDate[0]);
-            Article_id = int.Parse(articleParams[5].ToString());
+            Article_id = int.Parse(articleParams[4].ToString());
         }
 
         public string Title { get; private set; }
@@ -23,8 +22,6 @@ namespace Sem.ModelsTables
         public string Image { get; private set; }
 
         public string Description { get; private set; }
-
-        public string Short_description { get; private set; }
 
         public DateTime Date { get; private set; }
 
